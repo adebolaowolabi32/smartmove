476b329bfea7617ef200203e16be2dfee177f047
Earnest Erihbra Suru <serihbrah@gmail.com>
2020-08-30 18:55:51 +0100

    added fileParser backend for handling entity uploads on smartmove

--- src/main/java/com/interswitch/smartmoveserver/util/FilefileOpsUtil.java
+++ src/main/java/com/interswitch/smartmoveserver/util/FilefileOpsUtil.java
@@ -1,12 +1,13 @@
 package com.interswitch.smartmoveserver.util;
 
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
 
 public class FilefileOpsUtil {
 
@@ -45,4 +46,5 @@ public class FilefileOpsUtil {
         return convFile;
 
     }
+
 }
