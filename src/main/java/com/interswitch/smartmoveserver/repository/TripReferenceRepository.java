6351dbf268db61408adb0d9309f92d9c7d4b6ede
Adebola Owolabi <adebola.owolabi@interswitchgroup.com>
2020-11-05 18:08:44 +0100

    implementation of trip prefix configuration

---
+++ src/main/java/com/interswitch/smartmoveserver/repository/TripReferenceRepository.java
@@ -0,0 +1,14 @@
+package com.interswitch.smartmoveserver.repository;
+
+
+import com.interswitch.smartmoveserver.model.TripReference;
+import com.interswitch.smartmoveserver.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TripReferenceRepository extends CrudRepository<TripReference, Long> {
+    TripReference findByOwner(User owner);
+    void deleteByOwner(User owner);
+
+}
