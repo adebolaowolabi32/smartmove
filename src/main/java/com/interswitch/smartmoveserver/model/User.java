9473c2198b52ccd14be0bf44fb73580b6f5a50e5
Adebola Owolabi <adebola.owolabi@interswitchgroup.com>
2020-12-07 02:25:19 +0100

    Self Service Sign Up Implementation

--- src/main/java/com/interswitch/smartmoveserver/model/User.java
+++ src/main/java/com/interswitch/smartmoveserver/model/User.java
@@ -47,7 +47,7 @@ public class User extends AbstractAuditEntity<String> implements Auditable<Long>
     private String address;
 
     @Column(unique=true)
-    @NotBlank(message = "Mobile number is required.")
+    //@NotBlank(message = "Mobile number is required.")
     @Length(min = 5, max = 30, message = "Mobile number must be between 5 and 30 characters long.")
     private String mobileNo;
 
