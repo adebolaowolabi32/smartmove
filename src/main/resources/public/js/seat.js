5311d4ba1cbfa82dedb89e05ce80a56b48f62eeb
mmekut-mfon.edet <mmekut-mfon.edet@interswitchgroup.com>
2021-01-13 14:05:11 +0100

    four seater selection bug refactor

--- src/main/resources/public/js/seat.js
+++ src/main/resources/public/js/seat.js
@@ -1,29 +1,30 @@
 let seatNumber=[];
 function seatBooking(id){
-
+console.log(id);
  let elements = document.querySelectorAll(".seatLabel");
  let inputField= document.getElementById('captureData');
  let submitButton= document.getElementById('seatButton');
 
   for (let elem of elements) {
       if(elem.id===id){
-
+         console.log(elem.style.background)
        if(elem.style.background==='red'){
         return this;
        }
        if(elem.style.background==='white'){
+
              seatNumber.push(id);
              elem.style.background='#74C965';
              let seatData=seatNumber.join();
              inputField.value=seatData;
 
         }
-         if(elem.style.background==='none'){
-                    seatNumber.push(id);
-                    elem.style.background='#74C965';
-                    let seatData=seatNumber.join();
-                    inputField.value=seatData;
-                              }
+//         if(elem.style.background==='none'){
+//                    seatNumber.push(id);
+//                    elem.style.background='#74C965';
+//                    let seatData=seatNumber.join();
+//                    inputField.value=seatData;
+//                              }
         else{
 
 
