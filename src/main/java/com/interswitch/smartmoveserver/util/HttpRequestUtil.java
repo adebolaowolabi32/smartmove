873b376e9cdbbff46fa657c16c1625dc5e051228
Earnest Erihbra Suru <serihbrah@gmail.com>
2021-01-20 07:59:34 +0100

    fixed get-started link on index page.

---
+++ src/main/java/com/interswitch/smartmoveserver/util/HttpRequestUtil.java
@@ -0,0 +1,52 @@
+package com.interswitch.smartmoveserver.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+@Component
+public class HttpRequestUtil {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    private static final String[] IP_HEADER_CANDIDATES = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "HTTP_VIA",
+            "REMOTE_ADDR"
+    };
+
+    public String getClientIpAddress() {
+
+        if (request== null) {
+            return "0.0.0.0";
+        }
+
+        for (String header : IP_HEADER_CANDIDATES) {
+            String ipList = request.getHeader(header);
+            if (ipList != null && ipList.length() != 0 && !"unknown".equalsIgnoreCase(ipList)) {
+                String ip = ipList.split(",")[0];
+                return ip;
+            }
+        }
+
+       return request.getRemoteAddr();
+//        HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.getRequestAttributes())).getRequest();
+//        String ip = Optional.ofNullable(request.getHeader("X-FORWARDED-FOR")).orElse(request.getRemoteAddr());
+//        if (ip.equals("0:0:0:0:0:0:0:1")) ip = "127.0.0.1";
+//       // Assert.isTrue(ip.chars().filter($ -> $ == '.').count() == 3, "Illegal IP: " + ip);
+//        return ip;
+    }
+}
