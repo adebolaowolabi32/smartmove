a7b1d51935df177dae3f50bfa04f1c910113fa65
Earnest Erihbra Suru <serihbrah@gmail.com>
2021-02-08 10:27:20 +0100

    -exposed api to findSeatsByVehicleID -hid some fields(using @jsonIgnore) on seat model that are not required to be seen by Api clients

--- src/main/java/com/interswitch/smartmoveserver/api/TicketBookingApi.java
+++ src/main/java/com/interswitch/smartmoveserver/api/TicketBookingApi.java
@@ -1,14 +1,15 @@
 package com.interswitch.smartmoveserver.api;
 
+import com.interswitch.smartmoveserver.model.Seat;
 import com.interswitch.smartmoveserver.model.request.ScheduleSearchRequest;
 import com.interswitch.smartmoveserver.model.response.ScheduleSearchResult;
+import com.interswitch.smartmoveserver.service.SeatService;
 import com.interswitch.smartmoveserver.service.TicketService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/booking")
@@ -17,8 +18,17 @@ public class TicketBookingApi {
     @Autowired
     TicketService ticketService;
 
+    @Autowired
+    SeatService seatService;
+
+
     @PostMapping(value = "/search", produces = "application/json")
     private ScheduleSearchResult findByOwnerId(@Validated @RequestBody ScheduleSearchRequest scheduleSearch) {
         return ticketService.findBookingFromApi(scheduleSearch);
+    }
+
+    @GetMapping(value = "/seats", produces = "application/json")
+    private Set<Seat> findSeatsByVehicleId(@Validated @RequestParam("vehicleId") long  vehicleId) {
+        return seatService.findSeatsByVehicleId(vehicleId);
     }
 }
